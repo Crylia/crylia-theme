7bd133722f946a71ab7282c3029649e231556706
Toni500github <toni500minecraft@gmail.com>
2023-03-25 18:47:26 +0100

    changing from alacritty to kitty

--- autoinstall_arch.sh
+++ autoinstall_arch.sh
@@ -6,17 +6,17 @@ echo "	   /   | / / / /_  __/ __ \/ ____/ __ \ \/ / /   /  _/   | "
 echo "	  / /| |/ / / / / / / / / / /   / /_/ /\  / /    / // /| | "
 echo "	 / ___ / /_/ / / / / /_/ / /___/ _, _/ / / /____/ // ___ | "
 echo "	/_/  |_\____/ /_/  \____/\____/_/ |_| /_/_____/___/_/  |_| "
-echo " "   
+echo " "
 
 yay -S awesome-git rofi-git picom-jonaburg-git ttf-meslo-nerd-font-powerlevel10k
 
-sudo pacman -S papirus-icon-theme pulseaudio-alsa upower bluez bluez-utils xorg-setxkbmap xfce4-power-manager playerctl lightdm light-locker alacritty thunar flameshot
+sudo pacman -S papirus-icon-theme pulseaudio-alsa upower bluez bluez-utils xorg-setxkbmap xfce4-power-manager playerctl lightdm light-locker kitty thunar flameshot
 
 cd
 git clone --recurse-submodules https://github.com/Crylia/crylia-theme
 cd crylia-theme
-[ ! -d ~/.config/awesome ] && cp -r awesome ~/.config/. || cp -r ~/.config/awesome/ ~/.config/.awesome-backup && cp -r awesome ~/.config/.  
-[ ! -f ~/.config/picom.conf ] && cp picom.conf ~/.config/. || cp ~/.config/picom.conf ~/.config/.picom.conf.backup && cp picom.conf ~/.config/. 
+[ ! -d ~/.config/awesome ] && cp -r awesome ~/.config/. || cp -r ~/.config/awesome/ ~/.config/.awesome-backup && cp -r awesome ~/.config/.
+[ ! -f ~/.config/picom.conf ] && cp picom.conf ~/.config/. || cp ~/.config/picom.conf ~/.config/.picom.conf.backup && cp picom.conf ~/.config/.
 [ ! -d ~/.config/rofi ] && cp -r rofi ~/.config/. || cp -r ~/.config/rofi ~/.config/.rofi-backup && cp -r rofi ~/.config/.
 [ ! -d ~/.config/alacritty ] && cp -r alacritty ~/.config/. || cp -r ~/.config/alacritty ~/.config/.alacritty && cp -r alacritty ~/.config/.
 
--- autoinstall_pop_os.sh
+++ autoinstall_pop_os.sh
@@ -6,17 +6,17 @@ echo "	   /   | / / / /_  __/ __ \/ ____/ __ \ \/ / /   /  _/   | "
 echo "	  / /| |/ / / / / / / / / / /   / /_/ /\  / /    / // /| | "
 echo "	 / ___ / /_/ / / / / /_/ / /___/ _, _/ / / /____/ // ___ | "
 echo "	/_/  |_\____/ /_/  \____/\____/_/ |_| /_/_____/___/_/  |_| "
-echo " "                                                          
+echo " "
 
 DIR=crylia
 # dependencies for meson, ninja, rofi, awesome and all extra optional packages
-sudo apt -y install meson ninja-build cmake cmake-data pkg-config papirus-icon-theme xorg build-essential git make autoconf automake flex bison check go-md2man doxygen cppcheck ohcount pulseaudio-utils upower bluez xorg xfce4-power-manager playerctl lightdm light-locker libxcb-ewmh-dev libxcb-xfixes0-dev libev-dev libxcb-damage0-dev libxcb-sync-dev libxcb-composite0-dev libxcb-present-dev uthash-dev libconfig-dev libgl-dev ncurses-term bison flex check
+sudo apt -y install meson ninja-build cmake cmake-data pkg-config papirus-icon-theme xorg build-essential git make autoconf automake flex bison check go-md2man doxygen cppcheck ohcount pulseaudio-utils upower bluez xorg xfce4-power-manager playerctl lightdm light-locker libxcb-ewmh-dev libxcb-xfixes0-dev libev-dev libxcb-damage0-dev libxcb-sync-dev libxcb-composite0-dev libxcb-present-dev uthash-dev libconfig-dev libgl-dev kitty bison flex check
 
 # fonts
 cd
 mkdir -p $DIR
 mkdir -p .fonts && cd .fonts
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf 
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
@@ -50,8 +50,8 @@ sudo ninja -C build install
 cd ~/$DIR
 git clone --recurse-submodules https://github.com/Crylia/crylia-theme
 cd crylia-theme
-[ ! -d ~/.config/awesome ] && cp -r awesome ~/.config/. || cp -r ~/.config/awesome/ ~/.config/.awesome-backup && cp -r awesome ~/.config/.  
-[ ! -f ~/.config/picom.conf ] && cp picom.conf ~/.config/. || cp ~/.config/picom.conf ~/.config/.picom.conf.backup && cp picom.conf ~/.config/. 
+[ ! -d ~/.config/awesome ] && cp -r awesome ~/.config/. || cp -r ~/.config/awesome/ ~/.config/.awesome-backup && cp -r awesome ~/.config/.
+[ ! -f ~/.config/picom.conf ] && cp picom.conf ~/.config/. || cp ~/.config/picom.conf ~/.config/.picom.conf.backup && cp picom.conf ~/.config/.
 [ ! -d ~/.config/rofi ] && cp -r rofi ~/.config/. || cp -r ~/.config/rofi ~/.config/.rofi-backup && cp -r rofi ~/.config/.
 [ ! -d ~/.config/alacritty ] && cp -r alacritty ~/.config/. || cp -r ~/.config/alacritty ~/.config/.alacritty && cp -r alacritty ~/.config/.
 
