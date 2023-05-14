2b756c563b0ab5bfeea33d3371cdd4b1f13c5b62
Toni500github <toni500minecraft@gmail.com>
2023-05-14 19:01:41 +0200

    for arch too

--- autoinstall_arch.sh
+++ autoinstall_arch.sh
@@ -15,10 +15,9 @@ sudo pacman -S papirus-icon-theme pulseaudio-alsa upower bluez bluez-utils xorg-
 cd
 git clone --recurse-submodules https://github.com/Crylia/crylia-theme
 cd crylia-theme
-[ ! -d ~/.config/awesome ] && cp -r awesome ~/.config/. || cp -r ~/.config/awesome/ ~/.config/.awesome-backup && cp -r awesome ~/.config/.
-[ ! -f ~/.config/picom.conf ] && cp picom.conf ~/.config/. || cp ~/.config/picom.conf ~/.config/.picom.conf.backup && cp picom.conf ~/.config/.
-[ ! -d ~/.config/rofi ] && cp -r rofi ~/.config/. || cp -r ~/.config/rofi ~/.config/.rofi-backup && cp -r rofi ~/.config/.
-[ ! -d ~/.config/kitty ] && cp -r kitty ~/.config/. || cp -r ~/.config/kitty ~/.config/.kitty-backup && cp -r kitty ~/.config/.
-
+[ ! -d ~/.config/awesome ] 	&& cp -r awesome ~/.config/. || cp -r ~/.config/awesome/ ~/.config/.awesome-backup && rm -rf ~/.config/awesome/ && cp -r awesome ~/.config/.
+[ ! -f ~/.config/picom.conf ] 	&& cp picom.conf ~/.config/. || cp    ~/.config/picom.conf ~/.config/.picom.conf.backup && cp -f picom.conf ~/.config/.
+[ ! -d ~/.config/rofi ] 	&& cp -r rofi ~/.config/.    || cp -r ~/.config/rofi ~/.config/.rofi-backup && rm -rf ~/.config/rofi && cp -r rofi ~/.config/.
+[ ! -d ~/.config/kitty ] 	&& cp -r kitty ~/.config/.   || cp -r ~/.config/kitty ~/.config/.kitty-backup && rm -rf ~/.config/kitty && cp -r kitty ~/.config/.
 
 echo " ===== make sure to logout/reboot and select awesome desktop ====== "
